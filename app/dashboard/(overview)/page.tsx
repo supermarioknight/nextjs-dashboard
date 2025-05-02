3f7a8187b3d64fda055a3f26ad1154cba4a54cda
supermarionight <coderscope056@gmail.com>
2025-05-01 22:04:32 -0300

    fix: Install ESLint & fixing ESLint warnings

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from "react";
-import { Card } from "../../ui/dashboard/cards"
 import RevenueChart from "../../ui/dashboard/revenue-chart"
 import LatestInvoices from "../../ui/dashboard/latest-invoices"
 import { lusitana } from "../../ui/fonts"
